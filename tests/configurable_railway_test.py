af10ea86f468cb86c8502d3d222574f4f5b55131
Lennart Fridén <lennart@devl.se>
2022-02-03 15:11:37 +0100

    Rework test to avoid rerasing an error for clarity

--- tests/configurable_railway_test.py
+++ tests/configurable_railway_test.py
@@ -1,19 +1,23 @@
-import pytest
 from python_on_rails.result import as_result
 from python_on_rails.railway import Railway
 
 
 def test_failed_process_with_custom_error_handler():
-    def raise_error(result):
-        raise result.error
+    def handle_error(result):
+        return (
+            f"Got a <{result.error.__class__.__name__}> with the message '{result.error.args[0]}'"
+        )
 
-    with pytest.raises(RuntimeError, match="Failed to download"):
-        Railway(failed_download, parse, output).run(failure_handler=raise_error)
+    result = Railway(failed_download, parse, output).run(failure_handler=handle_error)
+    assert result == "Got a <RuntimeError> with the message 'Failed to download'"
 
 
 @as_result()
 def download(result):
-    return [{"date": "2022-01-19", "clicks": 13}, {"date": "2022-01-20", "clicks": 37}]
+    return [
+        {"date": "2022-01-19", "clicks": 13},
+        {"date": "2022-01-20", "clicks": 37},
+    ]
 
 
 @as_result()
